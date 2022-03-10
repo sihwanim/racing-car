c014853801f9ed2e99088dad4632e48224fe23bb
sihwa1019 <epretty@naver.com>
2022-03-10 18:05:01 +0900

    FEAT : 1~10까지 랜덤 수 생성

---
+++ src/main/java/CarRacing/Utils/RandomUtils.java
@@ -0,0 +1,13 @@
+package CarRacing.Utils;
+
+import java.util.Random;
+
+public class RandomUtils {
+    private static final Random RANDOM = new Random();
+
+    private RandomUtils(){
+    }
+    public  static int nextNumber(){
+        return RANDOM.nextInt(10);
+    }
+}
