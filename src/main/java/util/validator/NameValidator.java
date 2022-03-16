3fa52b215936163a98253aff96e73622e29834ff
gyp03236 <gyp03236@naver.com>
2022-03-17 01:28:18 +0900

    refactor: 입력된 자동차가 2대 이상인지 확인하는 코드 추가

--- src/main/java/util/validator/NameValidator.java
+++ src/main/java/util/validator/NameValidator.java
@@ -7,6 +7,7 @@ public class NameValidator extends Validator{
 	private static final String DELIMITER = ",";
 	private static final String DUPLICATED_NAME_MESSAGE = "중복된 이름이 존재합니다.";
 	private static final String NAME_LENGTH_MESSAGE = "이름은 1자 이상 5자 이하로 입력해야 합니다.";
+	private static final String CANNOT_RACE_MESSAGE = "자동차가 2대 미만입니다.";
 	private static final int MIN_LENGTH = 1;
 	private static final int MAX_LENGTH = 5;
 
@@ -15,6 +16,7 @@ public class NameValidator extends Validator{
 		isNull(input);
 		isEmpty(input);
 		isDuplicated(input);
+		countCar(input);
 	}
 
 	// 이름 하나씩 확인
@@ -30,6 +32,13 @@ public class NameValidator extends Validator{
 		if(cars.size() != cnt){
 			throw new IllegalArgumentException(DUPLICATED_NAME_MESSAGE);
 		}
+	}
+
+	public static void countCar(String input){
+		List<String> cars = Arrays.asList(input.split(DELIMITER));
+		if(cars.size() < 2)
+			throw new IllegalArgumentException(CANNOT_RACE_MESSAGE);
+
 	}
 
 	public static void isRange(String input){
