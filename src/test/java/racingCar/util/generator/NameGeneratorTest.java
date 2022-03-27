5c6ddc7b54a1e1514150f69543b66969eaf7fc11
gyp03236 <gyp03236@naver.com>
2022-03-27 21:16:18 +0900

    refator: 이름 생성 test 수정

--- src/test/java/racingCar/util/generator/NameGeneratorTest.java
+++ src/test/java/racingCar/util/generator/NameGeneratorTest.java
@@ -1,6 +1,7 @@
 package racingCar.util.generator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,13 @@ public class NameGeneratorTest {
 	void 이름정상입력(){
 		assertThat(NameGenerator.generatorCarsName("pobi,crong,honux"))
 			.contains("pobi", "crong", "honux");
+	}
+
+	@DisplayName("이름이 잘못 입력되었을 때를 확인한다.")
+	@Test
+	void 이름비정상입력(){
+		assertThatThrownBy(() -> {
+			NameGenerator.generatorCarsName("pobi12,crong,honux");
+		}).isInstanceOf(IllegalArgumentException.class);
 	}
 }
