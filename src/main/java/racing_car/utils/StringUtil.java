c35b585d56156e9e1d337e8d8dd70bbcd6d24f79
DyeonKim <devdoyeonkim@gmail.com>
2023-07-28 23:47:34 +0900

    refactor: 자동차 이름은 5자 이하만 가능하다.

--- src/main/java/racing_car/utils/StringUtil.java
+++ src/main/java/racing_car/utils/StringUtil.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class StringUtil {
-    public static String limitFiveOrLessLength(String s) {
-        return s.length() <= 5 ? s : s.substring(0, 5);
+    public static String limitLength(String s, int limitLen) {
+        return s.length() <= limitLen ? s : s.substring(0, limitLen);
     }
 
     public static String[] removeDuplicates(String[] sArray) {
--- src/main/java/racing_car/view/InputView.java
+++ src/main/java/racing_car/view/InputView.java
@@ -1,12 +1,13 @@
 package racing_car.view;
 
-import static racing_car.utils.StringUtil.limitFiveOrLessLength;
+import static racing_car.utils.StringUtil.limitLength;
 import static racing_car.utils.StringUtil.removeDuplicates;
 
 import java.util.Scanner;
 
 public class InputView {
-    private Scanner scanner;
+    private final int LIMIT_LENGTH = 5;
+    private final Scanner scanner;
 
 
     public InputView(Scanner scanner) {
@@ -17,7 +18,7 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String[] carNames = scanner.nextLine().split(" *, *");
         for (int i = 0; i < carNames.length; i++) {
-            carNames[i] = limitFiveOrLessLength(carNames[i]);
+            carNames[i] = limitLength(carNames[i], LIMIT_LENGTH);
         }
         return removeDuplicates(carNames);
     }
