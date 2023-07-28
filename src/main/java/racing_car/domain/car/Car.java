d96ed6a7dafee5a5f74285066553c898ddf804ea
DyeonKim <devdoyeonkim@gmail.com>
2023-07-29 01:06:09 +0900

    feat: 4 이상이면 전진, 3 이하이면 멈춤.

--- src/main/java/racing_car/domain/car/Car.java
+++ src/main/java/racing_car/domain/car/Car.java
@@ -1,29 +1,35 @@
 package racing_car.domain.car;
 
 public class Car {
+    private final int MOVE_STANDARD = 4;
     private final String name;
-    private int goCount;
+    private int position;
 
 
     public Car(String name) {
         this.name = name;
-        this.goCount = 0;
+        this.position = 0;
     }
 
-    public Car(String name, int goCount) {
+    public Car(String name, int position) {
         this.name = name;
-        this.goCount = goCount;
+        this.position = position;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getGoCount() {
-        return goCount;
+    public int getPosition() {
+        return position;
     }
 
-    public void go() {
-        goCount++;
+    public void go(boolean canGo) {
+        if (canGo)
+            position++;
+    }
+
+    public boolean isGo(int standard) {
+        return standard >= MOVE_STANDARD;
     }
 }
