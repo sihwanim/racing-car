ca45e828785fcc82eca1ced4b37f68b469c94146
sihwa1019 <epretty@naver.com>
2023-08-08 18:41:31 +0900

    feat: 초기세팅

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,15 +1,18 @@
 public class Calculator {
-    public int add(int x, int y){
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
-    public int subtract(int x, int y){
+
+    public int subtract(int x, int y) {
         return x - y;
     }
-    public int multiply(int x, int y){
+
+    public int multiply(int x, int y) {
         return x * y;
     }
-    public int divide(int x, int y){
-        if(y==0) {
+
+    public int divide(int x, int y) {
+        if (y == 0) {
             throw new IllegalArgumentException("분모는 0이어서는 안됨");
         }
         return x / y;
