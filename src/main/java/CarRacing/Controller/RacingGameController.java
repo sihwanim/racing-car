52ecc708efede5eae70b2b2cba9184d70fb6bc28
sihwa1019 <epretty@naver.com>
2023-08-13 21:08:08 +0900

    feat: RacingGameController클래스 구현

---
+++ src/main/java/CarRacing/Controller/RacingGameController.java
@@ -0,0 +1,35 @@
+package CarRacing.Controller;
+
+import CarRacing.Domain.*;
+import CarRacing.View.RacingGameInputView;
+import CarRacing.View.RacingGameOutputView;
+
+import java.util.List;
+
+public class RacingGameController {
+    RacingGameOutputView racingGameOutputView = new RacingGameOutputView();
+
+    public RacingGameController() {
+
+    }
+
+    public void play(){
+        RacingGame racingGame = new RacingGame();
+        String inputName = RacingGameInputView.MessageName();
+        Cars cars = RacingGameFactory.inputCars(inputName);
+        int inputNum = RacingGameInputView.MessageNumber();
+
+        for(int i = 0; i < inputNum; i++){
+            racingGame.moveCars(cars);
+            racingGameOutputView.printTrialResult(cars);
+            System.out.println();
+        }
+
+        showResult(cars);
+    }
+
+    public void showResult(Cars cars){
+        racingGameOutputView.printWinner(cars);
+    }
+
+}
