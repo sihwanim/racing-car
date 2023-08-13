68498d687019e4f5e6d6755328450cdcd419b44b
sihwa1019 <epretty@naver.com>
2023-08-13 21:07:15 +0900

    feat: 자동차를 움직이고 우승자를 찾는 RacingGame 클래스 구현

---
+++ src/main/java/CarRacing/Domain/RacingGame.java
@@ -0,0 +1,41 @@
+package CarRacing.Domain;
+
+import CarRacing.Utils.RandomUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame {
+    RandomUtils randomUtils = new RandomUtils();
+    private final int GO_STOP_NUM = 4;
+
+    public Cars moveCars(Cars cars){
+       for(int i = 0 ; i < cars.cars.size(); i++){
+           moveCar(cars.cars.get(i));
+       }
+       return cars;
+    }
+
+    public Car moveCar(Car car){
+        int num = randomUtils.goRandomNumber();
+        if(num >= GO_STOP_NUM)
+            car.position = new Position(car.position.getPosition() + 1);
+
+        return car;
+    }
+
+    public int findMaxPosition(Cars cars){
+       return cars.cars.stream().mapToInt(Car::getPosition).max().orElse(0);
+    }
+
+    public String findWinners(Cars cars) {
+        int maxPosition = findMaxPosition(cars);
+
+        return cars.cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.joining(","));
+
+
+    }
+}
